9623233d7bfe5f18a297f8b4ecd7c997d1cc8eb5
andrew <andrew@oopsolutions>
2014-10-09 15:53:23 +0100

    changed test page to display in web browser

--- ch2_exercises/product_test.php
+++ ch2_exercises/product_test.php
@@ -1,21 +1,28 @@
-<?php
+<!DOCTYPE html>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>This is Product Class Test page</h1>
 
+        <?php
 // include the class file
-require_once '../Ch2/Product.php';
+        require_once '../Ch2/Product.php';
 
 // create an instance of the Ch2_Product class
-$product = new Ch2_Product();
+        $product = new Ch2_Product();
 
 // display the $_type property
-//echo $product->_type;
+        echo $product->_type;
 
 // try this too
-$product->_type = 'DVD';
+        $product->_type = 'DVD';
+        ?>
+    </body>
+</html>
+
 
 
