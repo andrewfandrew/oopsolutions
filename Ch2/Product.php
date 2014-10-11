fe688d3abc2158a4afa5dd067e2c5ac96ffa7b97
andrew <andrew@oopsolutions>
2014-10-11 19:39:54 +0100

    Have used constructor method.

--- Ch2/Product.php
+++ Ch2/Product.php
@@ -3,7 +3,28 @@
 
 class Ch2_Product {
 // properties go here
-public $_type = 'Book';
+protected $_type;
+protected $_title;
 
+// constructor
+public function __construct($type, $title) {
+    // The value of $_type is now being set by the constructor 
+    // which replaces the setter method 
+    // Use a getter method to fetch teh value of teh $_title property
+    $this->_type = $type;
+    $this->_title = $title;
 }
+    
+    // methods defined here
+    public function getProductType()
+    {
+        return $this->_type;
+    }
+    public function getTitle()
+    {
+        return $this->_title;
+    }
+}
+
+
 
--- ch2_exercises/product_test.php
+++ ch2_exercises/product_test.php
@@ -3,7 +3,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>AF: Safari books, 'PHP Object Oriented Solutions'    </title>
     </head>
     <body>
         <h1>This is Product Class Test page</h1>
@@ -13,13 +13,21 @@
         require_once '../Ch2/Product.php';
 
 // create an instance of the Ch2_Product class
-        $product = new Ch2_Product();
+//       $product = new Ch2_Product();
 
 // display the $_type property
-        echo $product->_type;
+//        echo $product->_type;
 
 // try this too
-        $product->_type = 'DVD';
+//        $product->_type = 'DVD';
+        
+        $product1 = new Ch2_Product('Book', 'PHP Object Oriented PHP Solutions');
+        $product2 = new Ch2_Product('DVD', 'The Guns of Navaronne');
+        
+        echo '<p>$product1 is a ' . $product1->getProductType();
+        echo ' called "' . $product1->getTitle(). '"</p>';
+        echo '<p>$product2 is a ' . $product2->getProductType();
+        echo ' called "' . $product2->getTitle() .  '"</p>';
        
         ?>
     </body>
